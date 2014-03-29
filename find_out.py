326b77b726b9041205ec24c78d753e0dd618c187
poeticg33k <poetic1@gmail.com>
2014-03-28 22:40:54 -0600

    added sleep

--- find_out.py
+++ find_out.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import subprocess
 import sys
+import time
 OS = sys.platform
 fs_encoding  = sys.getfilesystemencoding()
 prefix = sys.prefix
@@ -9,7 +10,11 @@ prefix = sys.prefix
 #print(sys.path)
 #print fs_encoding
 #print prefix
-
+#print(sys.getprofile())
+#print(sys.gettrace())
+#print(sys.getdlopenflags())
+#print(sys.getcheckinterval())
+#sys.exit()
 #first attempt
 def finder(): 
 	
@@ -40,6 +45,7 @@ def wifi_scan():
 	if OS.startswith('darwin'):
 
 		subprocess.call(["airport", "-s"])
+		subprocess.call(["airport", "-I"])
 
 	elif OS.startswith('linux'):
 
@@ -47,14 +53,41 @@ def wifi_scan():
 	else:
 		sys.exit()
 
+def folder():
+	
+	if OS.startswith('darwin'):
+		subprocess.call(["mkdir", "mine"])
+		print 'folder created'
+		time.sleep(0)
+		subprocess.call(["rm", "-rf", "mine"])
+		print 'folder removed'
+	
+	elif OS.startswith('linux'):
+		subprocess.call(["mkdir", "mine"])
+		print 'folder created'
+		time.sleep(0)
+		subprocess.call(["rm", "-rf", "mine"])
+		print 'folder removed'
+	
+	else:
+		sys.exit()
+
+
+
 def input():
 
 	if OS.startswith('darwin'):
 
 		answer = raw_input('i see you are on a MAC, would you like to see what i know?:')
 		if answer == 'y':
-			#finder()
+			finder()
+			time.sleep(0)
 			wifi_scan()
+			time.sleep(0)
+			subprocess.call(["pwd"])
+			time.sleep(0)
+			folder()
+			
 		elif answer == 'n':
 			print 'fine then i will keep my secrets!'
 		else:
@@ -62,11 +95,17 @@ def input():
 
 	elif OS.startswith('linux'):
 
-		answer2 = raw_input('i see you are using linux, would you like to see what i know?:')
-		if answer2 == 'y':
-			#finder()
+		answer0 = raw_input('i see you are using linux, would you like to see what i know?:')
+		if answer0 == 'y':
+			finder()
+			time.sleep(0)
 			wifi_scan()
-		elif answer2 == 'n':
+			time.sleep(0)
+			subprocess.call(["pwd"])
+			time.sleep(0)
+			folder()
+
+		elif answer0 == 'n':
 			print 'fine then i will keep my secrets!'
 		else:
 			sys.exit()
